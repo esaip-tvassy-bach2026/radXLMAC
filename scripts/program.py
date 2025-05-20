db1a9df496aaa7df69bb333b595f560aa64ac462
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-05-20 14:27:38 +0200

    Creating a load function
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- scripts/program.py
+++ scripts/program.py
@@ -1,7 +1,8 @@
 """
 Ce fichier contient le cerveau du programme. C-est le fichier central du programme.
 """
-from calculer import calcul
+# from calculer import calcul
+from chargement import charger
 
 def lancement_programme(forme,filename,sheet,MAC_B,nbr_rad,MAC_rad):
     """
@@ -12,3 +13,5 @@ def lancement_programme(forme,filename,sheet,MAC_B,nbr_rad,MAC_rad):
     assert isinstance(forme,str) and isinstance(filename,str), and isinstance(sheet,str) and isinstance(MAC_B,str) and isinstance(nbr_rad,int) and isinstance(MAC_rad,str), "ERREUR FATALE : Les données entrées ne sont pas dans les bons formats. Veuillez vérifier que vous avez entré que des chaînes de caractères et des nombres entiers. Le programme doit s-arrêter ici."
     assert forme=="O" or forme=="N", "ERREUR FATALE : Vous n-avez pas ou avez mal précisé si vous souhaitiez que le programme formatte les adresses MAC ou non. Relancez tout et corrigez votre saisie. Le programme doit s-arrêter ici."
     assert "xlsx" in filename, "ERREUR FATALE : Le fichier que vous avez indiqué n-a pas l-extension .xlsx. Veuillez relancer le programme puis corriger votre saisie. Le programme doit s-arrêter ici."
+    # Chargement du fichier demandé
+    file_loaded=charger(filename)
