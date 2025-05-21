090c1f1277674272ca2e670dea640b08c32b1f99
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-05-21 10:55:31 +0200

    Creating the function copie
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- scripts/copier.py
+++ scripts/copier.py
@@ -9,4 +9,12 @@ def copie(fichier,nom_destination):
 	Entrée : fichier(str), nom_destination(str)
 	Sortie : Pas de sortie, le fichier est copié et collé avec son nouveau nom.
 	"""
-	assert isinstance(fichier,str) and isinstance(nom_destination,str), "ERREUR FATALE :"
+	assert isinstance(fichier,str) and isinstance(nom_destination,str), "ERREUR FATALE : Il semble que vous avez une erreur de format dans le nom de votre archive initiale ou dans le nom de votre archive de destination. Veuillez fermer, redémarrer le programme et corriger votre saisie. Le programme doit s-arrêter ici."
+	assert "xlsx" in fichier, "ERREUR FATALE : Il semble que vous essayez d-utiliser cette fonction avec un fichier qui n-a pas l-extension XLSX. Ceci est impossible et n-est pas supporté par ce programme. Veuillez arrêter et redémarrer le programme afin de corriger votre saisie. Le programme doit s-arrêter ici."
+	assert "xlsx" in nom_destination, "ERREUR FATALE : Il semble que vous essayez d-utiliser cette fonction avec un fichier de destination qui n-a pas l-extension XLSX. Ceci est impossible et n-est pas supporté par ce programme. Veuillez arrêter et redémarrer le programme afin de corriger votre saisie. Le programme doit s-arrêter ici."
+	shutil.copy(fichier,nom_destination)
+
+print("\nERREUR FATALE : Vous ne pouvez pas exécuter cette fonction en mode standalone.")
+print("Merci de lancer uniquement le programme complet à partir du fichier main.py.")
+print("Le programme doit s-arrêter ici.")
+quit()
