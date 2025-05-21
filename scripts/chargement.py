b68fcb3f14241fa9bd2f4de87ab10ddc1f9437df
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-05-21 14:52:52 +0200

    Modifying the file to create the charger_feuilles function
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- scripts/chargement.py
+++ scripts/chargement.py
@@ -16,16 +16,23 @@ def charger(fichier,feuille):
 	tuple_final=(classeur,feuille)
 	return tuple_final
 
-def charger_colonnes(nb_col,col1,col2,col3=None,col4=None,col5=None,col6=None):
+def charger_colonnes(fichierc,ligne_depart=2,nb_radios,col_phys,col1,col2,col3=None,col4=None,col5=None,col6=None):
 	"""
 	Cette fonction permet de charger un certain nombre de colonnes d'un tableau situé dans une feuille.
 	Les colonnes sont sélectionnées par leur nom.
 	Ce programme ne peut pas supporter plus de 6 colonnes au total.
-	Entrée : nb_col(int), col1(str, colonne des adresses MAC physiques), col2(str, colonne des adresses MAC radio), col3(str,colonne supplémentaire), col4(str, colonne supplémentaire), col5(str, colonne supplémentaire), col6(str, colonne supplémentaire)
-	Sortie : contenu_col1(contenu de la colonne 1), contenu_col2(contenu de la colonne 2), contenu_col3(contenu de la colonne 3), contenu_col4(contenu de la colonne 4), contenu_col5(contenu de la colonne 5), contenu_col6(contenu de la colonne 6)
+	Entrée : fichierc (tpl), ligne_depart(int), nb_radios(int), col_phys(str), col1(str, colonne de l-adresse MAC radio 1), col2(str), col3(str), col4(str), col5(str), col6(str)
+	Sortie : super_tuple(tpl)
 	"""
-	assert isinstance(nb_col,int) and isinstance(col1,str) and isinstance(col2,str) and isinstance(col3,str) and isinstance(col4,str) and isinstance(col5,str) and isinstance(col6,str), "ERREUR FATALE : Il semble que les informations entrées en paramètre de cette fonction soient incorrectes. Veuillez redémarrer complètement le programme et corriger votre saisie. Le programme doit s-arrêter ici."
-	assert nb_col<=6, "ERREUR FATALE : Cette fonction n-est pas conçue pour supporter plus de 6 colonnes en même temps. Veuillez redémarrer le programme complètement et corriger votre saisie. Le programme doit s-arrêter ici."
+	assert isinstance(fichierc,tpl) and isinstance(ligne_depart,int) and isinstance(nb_radios,int) and isinstance(col_phys,str) and isinstance(col1,str) and isinstance(col2,str) and (isinstance(col3,str) or col3==None) and (isinstance(col4,str) or col4==None) and (isinstance(col5,str) or col5==None) and (isinstance(col6,str) or col6==None), "ERREUR FATALE : Les informations entrées en paramètres de cette fonction ne sont pas dans le bon format. Veuillez redémarrer complètement le programme et corriger votre saisie. Le programme doit s-arrêter ici."
+	assert nb_radios<=6, "ERREUR FATALE : Cette fonction ne peut prendre en charge un appareil qui a plus de 6 antennes radio. Veuillez redémarrer le programme et corriger votre saisie. Le programme doit s-arrêter ici."
+	# On charge le contenu de la feuille à modifier dans des variables.
+	contenu_feuille=fichierc[1]
+	# Ensuite, on créé un dictionnaire. Pour chaque nom de colonne, on lui associe un index qui correspond au nom de colonne Excel (A,B,C,D, etc.). Ainsi, Python peut savoir avec précision quelle colonne se situe où dans la feuille.
+	dico_colonnes={cellule.valeur:numero for numero,cellule in enumerate(contenu_feuille[ligne_depart])}
+	# Enfin, on remplit les variables avec les bonnes valeurs et on retourne un beau tuple.
+	colonne_phys=dico_colonnes[col_phys]
+	colonne1=dico_colonnes[col1]
 
 print("\nERREUR FATALE : Cette fonction ne peut pas être utilisée en mode standalone.")
 print("Veuillez lancer le programme complet à partir du fichier main.py.")
