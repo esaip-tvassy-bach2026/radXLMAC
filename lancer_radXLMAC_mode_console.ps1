33856cae7ccb2c5d47f9f2fb1ed420bd7bc95b8e
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-05-27 17:07:42 +0200

    Debugging the main file
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- lancer_radXLMAC_mode_console.ps1
+++ lancer_radXLMAC_mode_console.ps1
@@ -1,15 +1,21 @@
-# Ce script PowerShell permet de lancer radXLMAC en mode console dans PowerShell.
+ï»¿# Ce script PowerShell permet de lancer radXLMAC en mode console dans PowerShell.
 
-$python_path = Join-Path -Path $PSScriptRoot -ChildPath "special_python313_portable\python"
-$main_path = Join-Path -Path $PSScriptRoot -ChildPath "scripts\main.py"
+# Configurer PowerShell en UTF-8.
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::InputEncoding = [System.Text.Encoding]::UTF8
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+cmd /c chcp 65001 > $null
+
+$python_path = Join-Path -Path $PSScriptRoot -ChildPath "special_python313_portable\python.exe"
+$scripts_path = Join-Path -Path $PSScriptRoot -ChildPath "scripts"
 $python_ver = & $python_path --version
 $pip_ver = & $python_path -m pip --version
 
 Write-Host "==============================================="
 Write-Host "    Lancement de radXLMAC en mode console      "
 Write-Host "==============================================="
-Write-Host "Python utilisé : $python_path"
-Write-Host "Script lancé : $main_path"
+Write-Host "Python utilisÃ© : $python_path"
+Write-Host "Script lancÃ© : main.py"
 Write-Host "Version de Python : $python_ver"
 Write-Host "Version de Pip : $pip_ver"
 Write-Host ""
@@ -17,47 +23,54 @@ Write-Host "This program was made by Thomas VASSY--ROUSSEAU, intern at TIMCOD OU
 Write-Host "Copyright (C) TIMCOD SAS, 2025-2030. All rights reserved."
 Write-Host "Copyright (C) TIMCOD OUEST SAS, 2025-2030. All rights reserved."
 Write-Host ""
-Write-Host "Veuillez patienter... Procédons d-abord à des vérifications avant de commencer."
+Write-Host "Veuillez patienter... ProcÃ©dons d-abord Ã  des vÃ©rifications avant de commencer."
 Write-Host ""
 
-# Vérification de la présence des fichiers et dossiers nécessaires au bon fonctionnement
+# VÃ©rification de la prÃ©sence des fichiers et dossiers nÃ©cessaires au bon fonctionnement
 if (!(Test-Path $python_path)) {
-    Write-Host "ERREUR FATALE : Impossible de trouver où est Python !" -ForegroundColor Red
-    Write-Host "Merci de vérifier que tous les dossiers et fichiers téléchargés portent bien le même nom qu-à l-origine et contiennent bien tous les fichiers nécessaires." -ForegroundColor Yellow
+    Write-Host "ERREUR FATALE : Impossible de trouver oÃ¹ est Python !" -ForegroundColor Red
+    Write-Host "Merci de vÃ©rifier que tous les dossiers et fichiers tÃ©lÃ©chargÃ©s portent bien le mÃªme nom qu-Ã  l-origine et contiennent bien tous les fichiers nÃ©cessaires." -ForegroundColor Yellow
     Write-Host ""
-    Write-Host "Powershell doit s-arrêter ici." -ForegroundColor Red
+    Write-Host "Powershell doit s-arrÃªter ici." -ForegroundColor Red
     Pause
     exit 1
 }
-if (!(Test-Path $main_path)) {
+if (!(Test-Path $scripts_path)) {
     Write-Host "ERREUR FATALE : Impossible de trouver le cerveau du programme !" -ForegroundColor Red
-    Write-Host "Merci de vérifier que tous les dossiers et fichiers téléchargés portent bien le même nom qu-à l-origine et contiennent bien tous les fichiers nécessaires." -ForegroundColor Yellow
+    Write-Host "Merci de vÃ©rifier que tous les dossiers et fichiers tÃ©lÃ©chargÃ©s portent bien le mÃªme nom qu-Ã  l-origine et contiennent bien tous les fichiers nÃ©cessaires." -ForegroundColor Yellow
     Write-Host ""
-    Write-Host "Powershell doit s-arrêter ici." -ForegroundColor Red
+    Write-Host "Powershell doit s-arrÃªter ici." -ForegroundColor Red
     Pause
     exit 1
 }
 
 # Lancement de radXLMAC
 try {
-    & $python_path $main_path
+    Push-Location $scripts_path
+    & $python_path -m main
+    Pop-Location
     $exitCode = $LASTEXITCODE
     if ($exitCode -eq 0) {
         Write-Host ""
-        Write-Host "L-application s-est arrêtée. Traitement réalisé avec succès !" -ForegroundColor Green
+        Write-Host "L-application s-est arrÃªtÃ©e. Traitement rÃ©alisÃ© avec succÃ¨s !" -ForegroundColor Green
+        Write-Host ""
+        Pause
+        exit 0
     } else {
         Write-Host ""
-        Write-Host "L-application s-est arrêtée avec le code d-erreur suivant :" -ForegroundColor Red
-        Write-Host "$exitCode" -ForegroundColor Red
-        Write-Host "Merci de relancer complètement l-application. Cela peut parfois résoudre le problème." -ForegroundColor Yellow
+        Write-Host "L-application s-est arrÃªtÃ©e avec le code d-erreur suivant : $exitCode." -ForegroundColor Red
+        Write-Host "Merci de relancer complÃ¨tement l-application. Cela peut parfois rÃ©soudre le problÃ¨me." -ForegroundColor Yellow
+        Write-Host ""
+        Pause
+        exit 1
     }
 } catch {
     Write-Host ""
     Write-Host "ERREUR FATALE : Erreur inconnue (Unknown Error)." -ForegroundColor Red
-    Write-Host "Redémarrer l-application complètement peut parfois résoudre certains problèmes." -ForegroundColor Yellow
+    Write-Host "RedÃ©marrer l-application complÃ¨tement peut parfois rÃ©soudre certains problÃ¨mes." -ForegroundColor Yellow
     Write-Host ""
     Write-Host $_.Exception.Message -ForegroundColor Red
+    Write-Host ""
+    Pause
+    exit 1
 }
-
-# Permet de laisser la fenêtre ouverte à la fin du processus.
-Pause
